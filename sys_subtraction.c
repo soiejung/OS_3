2eedd8c359d6985b711352595cdda6bc64246975
soiejung <soiejung@naver.com>
2021-10-15 01:17:32 +0900

    뺄셈 연산해주는 시스템 콜 함수

---
+++ sys_subtraction.c
@@ -0,0 +1,15 @@
+#include <linux/kernel.h>
+#include <linux/linkage.h>
+#include <linux/syscalls.h>
+
+asmlinkage int sys_subtraction(int x, int y){
+
+	printk("x : %d",x);
+        printk("y : %d", y);
+	return x-y;
+}
+
+SYSCALL_DEFINE2(subtraction, int, x, int, y){
+
+	return sys_subtraction(x,y);
+}
